a0d70a2b788e6f51cbd1f1ab1a85ee31f5c81e37
loveyourvoice <96825672+loveyourvoice@users.noreply.github.com>
2024-08-15 11:04:02 +0300

    Исправил название папки

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,8 +11,6 @@ terraform {
     bucket     = "loveyourvoice-state-bucket"
     region     = "ru-central1"
     key        = "terraform.tfstate"
-    access_key = var.access_key
-    secret_key = var.secret_key
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
